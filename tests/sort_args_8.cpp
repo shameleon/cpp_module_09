54d4d33c462a7faf4f490eaf0392a45ce34f6b56
John Mouaikel <john.mouaikel@gamil.com>
2023-06-14 23:03:53 +0200

    test8 list to sort

--- tests/sort_args_8.cpp
+++ tests/sort_args_8.cpp
@@ -1,15 +1,13 @@
-/* sorted with vector */
+/* sorted with list */
 /**/
 
 #include <iostream>
-#include <vector>
+#include <list>
+#include <list>
 #include <limits>
 #include <cstdlib> 	  //std::stdtol
 #include <ctime>
 #include <algorithm>  // sort
-
-/* binary search range size */
-# define THRESHOLD	10
 
 /* colors */
 # define COL_RED    "\e[0;31m"
@@ -28,18 +26,18 @@
 # define COL_LRED  "\e[38:5:160m"
 # define COL_RES	"\e[0m"
 
-void	printOneVector(std::vector< int>  &vec, std::string color)
+void	printOneList(std::list< int>  &lst, std::string color)
 {
 	std::cout << COL_VIOL << std::endl;
-	for (std::vector< int> ::iterator it = vec.begin(); it != vec.end(); ++it)
+	for (std::list< int> ::iterator it = lst.begin(); it != lst.end(); ++it)
 		std::cout << color << *it << " ";
 	std::cout << COL_RES << std::endl;
 }
 
-void	printPairedVector(std::vector< std::pair <int, int> > &pairvec)
+void	printPairedList(std::list< std::pair <int, int> > &pairlst)
 {
 	std::cout << COL_VIOL << std::endl;
-	for (std::vector< std::pair <int, int> >::iterator it = pairvec.begin(); it != pairvec.end(); ++it)
+	for (std::list< std::pair <int, int> >::iterator it = pairlst.begin(); it != pairlst.end(); ++it)
 	{
 		std::cout << COL_GRN2 << it->first << " ";
 		std::cout << COL_VIOL << it->second;
@@ -48,117 +46,120 @@ void	printPairedVector(std::vector< std::pair <int, int> > &pairvec)
 	std::cout << COL_RES << std::endl;
 }
 
-int		binarySearch(int item, std::vector<int> &vec, int left, int right)
+// no subcript oveload for lists Random access of elements not possible.
+// as list is implemented as a double liked-list (access by traversing through iterators) 
+/*
+int		binarySearch(int item, std::list<int> &lst, int left, int right)
 {
 	if (right - left > THRESHOLD)
 	{
 		int		mid = left + (right - left) / 2;
-		if (item < vec[mid])
-			binarySearch(item, vec, left, mid);
-		else if (item > vec[mid])
-			binarySearch(item, vec, mid + 1, right);
-		else if (item == vec[mid])
+		if (item < lst[mid])
+			binarySearch(item, lst, left, mid);
+		else if (item > lst[mid])
+			binarySearch(item, lst, mid + 1, right);
+		else if (item == lst[mid])
 			return mid;
 	}
 	return left;
 }
-
-void	binarySearchInsert(std::vector<int> &elements_to_sort, std::vector<int> &vec)
+*/
+void	binarySearchInsert(std::list<int> &elements_to_sort, std::list<int> &lst)
 {
 	// std::cout << "_ _ _ _ _ _ _ _ _ _ _ _ _ _ " << std::endl;
-	// printOneVector(elements_to_sort, COL_GRN2);
-	// printOneVector(vec, COL_VIOL);
+	// printOneList(elements_to_sort, COL_GRN2);
+	// printOneList(lst, COL_VIOL);
 	if (elements_to_sort.size() == 0)
 		return;
-	std::vector< int> ::iterator it = vec.begin();
+	std::list< int> ::iterator it = lst.begin();
 	//std::cout << *it << std::endl;
-	// it != vec.end() not needed max element is already at the end
-	int	left = binarySearch(elements_to_sort.front(), vec, 0, vec.size() - 1);
-	it = it + left;
+	// it != lst.end() not needed max element is already at the end
+	//int	left = binarySearch(elements_to_sort.front(), lst, 0, lst.size() - 1);
+	//it = it + left;
 	while (elements_to_sort.front() > *it)
 		++it;
 	//std::cout << elements_to_sort.front() << " -> " << *it << std::endl;
-	vec.insert(it, elements_to_sort.front());
+	lst.insert(it, elements_to_sort.front());
 	elements_to_sort.erase(elements_to_sort.begin());
-	binarySearchInsert(elements_to_sort, vec);
+	binarySearchInsert(elements_to_sort, lst);
 }
 
-void	pushRightElements(std::vector<int> &elements_to_sort, std::vector<int> &vec,
-								std::vector< std::pair<int, int> > &pairvec)
+void	pushRightElements(std::list<int> &elements_to_sort, std::list<int> &lst,
+								std::list< std::pair<int, int> > &pairlst)
 {
-	std::vector< std::pair<int, int> >::iterator	it = pairvec.begin();
+	std::list< std::pair<int, int> >::iterator	it = pairlst.begin();
 
-	vec.push_back(it->first);
-	vec.push_back(it->second);
+	lst.push_back(it->first);
+	lst.push_back(it->second);
 	it++;
-	for(; it != pairvec.end(); ++it)
+	for(; it != pairlst.end(); ++it)
 	{
 		elements_to_sort.push_back(it->first);
-		vec.push_back(it->second);
+		lst.push_back(it->second);
 	}
 }
 
-void	sortPairsByRightElement(std::vector< std::pair<int, int> > &sortvec,
-								std::vector< std::pair<int, int> > &pairvec)
+void	sortPairsByRightElement(std::list< std::pair<int, int> > &sortlst,
+								std::list< std::pair<int, int> > &pairlst)
 {
-	while (pairvec.size())
+	while (pairlst.size())
 	{
-		std::vector< std::pair<int, int> >::iterator	it = pairvec.begin();
-		std::vector< std::pair<int, int> >::iterator	min_right_element = it;
+		std::list< std::pair<int, int> >::iterator	it = pairlst.begin();
+		std::list< std::pair<int, int> >::iterator	min_right_element = it;
 
-		for(; it != pairvec.end(); ++it)
+		for(; it != pairlst.end(); ++it)
 			if (it->second < min_right_element->second)
 				min_right_element = it;
-		sortvec.push_back(std::make_pair(min_right_element->first, min_right_element->second));
-		pairvec.erase(min_right_element);
-		sortPairsByRightElement(sortvec, pairvec);
+		sortlst.push_back(std::make_pair(min_right_element->first, min_right_element->second));
+		pairlst.erase(min_right_element);
+		sortPairsByRightElement(sortlst, pairlst);
 
 	}
 }
 
-void	groupElementsBySortedPairs(std::vector<int> &vec,
-								std::vector< std::pair<int, int> > &pairvec,
-								std::vector<int> &ending_singleton)
+void	groupElementsBySortedPairs(std::list<int> &lst,
+								std::list< std::pair<int, int> > &pairlst,
+								std::list<int> &ending_singleton)
 {
-	std::vector<int>::iterator		elements_to_sort = vec.begin();
-	std::vector<int>::iterator		right = vec.begin() + 1;
+	std::list<int>::iterator		it_left = lst.begin();
+	std::list<int>::iterator		it_right = lst.begin();
+	++it_right;
 
-	for (unsigned long i = 0; i < vec.size() - 1; ++i)
+	for (unsigned long i = 0; i < lst.size() - 1; ++i)
 	{
 		if (i % 2 == 0)
 		{
-			if (*elements_to_sort > *right)
-				pairvec.push_back(std::make_pair(*right, *elements_to_sort));
+			if (*it_left > *it_right)
+				pairlst.push_back(std::make_pair(*it_right, *it_left));
 			else
-				pairvec.push_back(std::make_pair(*elements_to_sort, *right));
+				pairlst.push_back(std::make_pair(*it_left, *it_right));
 		}
-		elements_to_sort++;
-		right++;
+		it_left++;
+		it_right++;
 	}
-	if (vec.size() % 2 == 1)
-		ending_singleton.push_back(*(elements_to_sort));
-	//vec.erase(vec.begin(), vec.end());
-	vec.clear();
+	if (lst.size() % 2 == 1)
+		ending_singleton.push_back(*(it_left));
+	//lst.erase(lst.begin(), lst.end());
+	lst.clear();
 }
 
-void	vecSort(std::vector<int> &vec)
+void	listSort(std::list<int> &lst)
 {
-	//int     size = vec.size();
-	std::vector< std::pair<int, int> >	pairvec, sortvec;
-	std::vector<int>					ending_singleton;
-	std::vector<int>					elements_to_sort;
+	std::list< std::pair<int, int> >	pairlst, sortlst;
+	std::list<int>					ending_singleton;
+	std::list<int>					elements_to_sort;
 	
-	groupElementsBySortedPairs(vec, pairvec, ending_singleton);
-	//printPairedVector(pairvec);
-	sortPairsByRightElement(sortvec, pairvec);
-	//printPairedVector(sortvec);
-	pushRightElements(elements_to_sort, vec, sortvec);
+	groupElementsBySortedPairs(lst, pairlst, ending_singleton);
+	//printPairedList(pairlst);
+	sortPairsByRightElement(sortlst, pairlst);
+	//printPairedList(sortlst);
+	pushRightElements(elements_to_sort, lst, sortlst);
 	if (ending_singleton.size())
-		elements_to_sort.push_back(ending_singleton[0]);
+		elements_to_sort.push_back(ending_singleton.front());
 	//std::cout << "# # # # # # # # # # # # # # " << std::endl;
-	//printOneVector(elements_to_sort, COL_GRN2);
-	//printOneVector(vec, COL_VIOL);
-	binarySearchInsert(elements_to_sort, vec);
+	//printOneList(elements_to_sort, COL_GRN2);
+	//printOneList(lst, COL_VIOL);
+	binarySearchInsert(elements_to_sort, lst);
 }
 
 /* timed sort function */
@@ -166,7 +167,7 @@ void	algo_sort(int argc, char**argv)
 {
 	int					int_min = 0;
 	int					int_max = std::numeric_limits<int>::max();
-	std::vector< int >	vec;  // vec(argc - 1);
+	std::list< int >	lst;
 
 	//std::cout << "Pushed : ";
 	for (int i = 1; i < argc; ++i)
@@ -180,13 +181,13 @@ void	algo_sort(int argc, char**argv)
 		else if (nb < static_cast<long>(int_min))
 			throw std::runtime_error("Error : negative number(s) not accepted.\n");
 		//std::cout << nb << " ";
-		vec.push_back(static_cast<int>(nb));
+		lst.push_back(static_cast<int>(nb));
 	}
 	std::cout << std::endl;
-	// display vector content
+	// display list content
 	std::cout << "Before : " << std::endl;
-	for (std::vector<int>::iterator it = vec.begin(); 
-			it != vec.end(); ++it)
+	for (std::list<int>::iterator it = lst.begin(); 
+			it != lst.end(); ++it)
 		std::cout << *it << " ";
 	std::cout << std::endl;
 
@@ -194,11 +195,11 @@ void	algo_sort(int argc, char**argv)
 	double		delta_t_us = 0;
 	start = std::clock();
 	/************* sorting function *************/ 
-	vecSort(vec);
+	listSort(lst);
 	stop = std::clock();
-	// display vector content
+	// display list content
 	std::cout << COL_ORANGE << "\nAfter : ";
-	printOneVector(vec, COL_BLG);
+	printOneList(lst, COL_BLG);
 	std::cout << std::endl;
 	delta_t_us += ( static_cast< double >(stop - start) * 1E6 ) / CLOCKS_PER_SEC;
 	std::cout << COL_ORANGE << "Time to process a range of " << (argc - 1) << " elements :  ";
@@ -231,9 +232,9 @@ include 0 or not ???
 List in C++ stores the elements at the non-contiguous memory location.
  It is considered a doubly linked list internally.
 
- A vector in C++ stores the elements at the contiguous memory location. 
+ A list in C++ stores the elements at the contiguous memory location. 
  It is considered to be a type of dynamic array internally.
-
+ 
 https://www.geeksforgeeks.org/binary-insertion-sort/
 
 
